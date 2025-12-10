938266d355cc349d360e4c45468941d6c15d5ff1
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-12-10 21:20:16 +0500

    Created

---
+++ Database.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const ConnectDb = async (MongoURL) => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/BlogScrap");
+    console.log("MongoDB is Connected");
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+module.exports = ConnectDb;
